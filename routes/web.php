aa0bf9dafc4fe74e4b588787654c8adbc81b0748
ashok <ashok.sharma3795@gmail.com>
2021-07-09 16:29:31 +0545

    form design

--- routes/web.php
+++ routes/web.php
@@ -14,24 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home.home');
 });
 
-Auth::routes();
+Route::get('home', function () {
+    return view('home.home');
+});
 
-Route::get('/home', 'HomeController@index')->name('home');
-
-Route::group(['prefix'=>'admin'], function(){
-
-	Route::get('dashboard', function(){
-		return view('admin.dashboard');
-	})->middleware(['CheckAdmin']);
-
-	Route::get('add-vendor','backend\VendorController@add');
-	Route::post('save-vendor','backend\VendorController@save');
-	Route::get('vendor','backend\VendorController@view');
-	Route::get('edit-vendor/{vendor_uuid}','backend\VendorController@edit');
-	Route::post('update-vendor/{vendor_uuid}','backend\VendorController@update');
-	Route::get('delete-vendor/{vendor_uuid}','backend\VendorController@delete');
-
+Route::get('link', function () {
+    return view('link.link');
 });
